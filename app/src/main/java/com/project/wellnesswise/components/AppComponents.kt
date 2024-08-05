656243bc8f5a4324dc8fc56bfc40eabad8c48f0b
zafar ali <zafaralyjamali722@gmail.com>
2024-08-05 17:54:46 +0500

    Added button comp

--- app/src/main/java/com/project/wellnesswise/components/AppComponents.kt
+++ app/src/main/java/com/project/wellnesswise/components/AppComponents.kt
@@ -2,9 +2,13 @@ package com.project.wellnesswise.components
 
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
@@ -12,6 +16,8 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -26,6 +32,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -185,4 +193,35 @@ fun ClickableTextComponent(value: String, onTextSelected: (String) -> Unit = {})
                    onTextSelected(span.item)
                }
            }})
+}
+
+
+@Composable
+fun ButtonComponent (value: String) {
+    Button(onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(48.dp),
+        contentPadding = PaddingValues(),
+        colors = ButtonDefaults.buttonColors(Color.Transparent)
+    ){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(48.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        listOf(
+                            colorResource(id = R.color.primary),
+                            colorResource(id = R.color.secondary)
+                        )
+                    ),
+                    shape = RoundedCornerShape(50.dp)
+                ),
+                   contentAlignment = Alignment.Center
+                )
+        {
+            Text(text = value, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+        }
+    }
 }
--- app/src/main/java/com/project/wellnesswise/screens/SignUpScreen.kt
+++ app/src/main/java/com/project/wellnesswise/screens/SignUpScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.project.wellnesswise.R
+import com.project.wellnesswise.components.ButtonComponent
 import com.project.wellnesswise.components.CheckBoxComponent
 import com.project.wellnesswise.components.HeadingTextComponent
 import com.project.wellnesswise.components.MyPasswordField
@@ -50,7 +51,10 @@ fun SignUpScreen (){
      CheckBoxComponent(value = stringResource(id = R.string.Agreement), onTextSelected = {
          WellnessWiseAppRouter.navigateTo(Screen.TermsAndConditionsScreen)
      })
+     Spacer(modifier = Modifier.height(10.dp))
+     ButtonComponent(value = stringResource(id = R.string.Register))
  }
+
     }
 }
 
